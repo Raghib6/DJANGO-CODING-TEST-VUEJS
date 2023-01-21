b8e46bcebccaee4f82e7a23e7a0b6502fca77578
Raghib Shahriar <raghibshahriar6@gmail.com>
2023-01-21 22:48:06 +0600

    Showing a list view of products table

--- django-coding-test/src/product/admin.py
+++ django-coding-test/src/product/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import Product, ProductVariant, ProductImage, ProductVariantPrice, Variant
 
-# Register your models here.
+
+class AdminVariant(admin.ModelAdmin):
+    list_display = ["title", "description", "active"]
+
+
+admin.site.register(Variant, AdminVariant)
+
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ["title", "sku", "description"]
+
+
+admin.site.register(Product, AdminProduct)
+
+
+class AdminProductVariant(admin.ModelAdmin):
+    list_display = ["variant_title", "variant", "product"]
+
+
+admin.site.register(ProductVariant, AdminProductVariant)
+
+admin.site.register(ProductImage)
+
+
+class AdminVariantPrice(admin.ModelAdmin):
+    list_display = [
+        "product_variant_one",
+        "product_variant_two",
+        "product_variant_three",
+        "price",
+        "stock",
+        "product",
+    ]
+
+
+admin.site.register(ProductVariantPrice, AdminVariantPrice)
--- django-coding-test/src/product/forms.py
+++ django-coding-test/src/product/forms.py
@@ -1,14 +1,35 @@
-from django.forms import forms, ModelForm, CharField, TextInput, Textarea, BooleanField, CheckboxInput
+from django.forms import (
+    forms,
+    ModelForm,
+    CharField,
+    TextInput,
+    Textarea,
+    BooleanField,
+    CheckboxInput,
+)
 
-from product.models import Variant
+from product.models import Variant, ProductVariantPrice
 
 
 class VariantForm(ModelForm):
     class Meta:
         model = Variant
-        fields = '__all__'
+        fields = "__all__"
         widgets = {
-            'title': TextInput(attrs={'class': 'form-control'}),
-            'description': Textarea(attrs={'class': 'form-control'}),
-            'active': CheckboxInput(attrs={'class': 'form-check-input', 'id': 'active'})
+            "title": TextInput(attrs={"class": "form-control"}),
+            "description": Textarea(attrs={"class": "form-control"}),
+            "active": CheckboxInput(
+                attrs={"class": "form-check-input", "id": "active"}
+            ),
         }
+
+
+class ProductForm(ModelForm):
+    class Meta:
+        model = ProductVariantPrice
+        fields = "__all__"
+        # widgets = {
+        #     'title': TextInput(attrs={'class': 'form-control'}),
+        #     'description': Textarea(attrs={'class': 'form-control'}),
+        #     'active': CheckboxInput(attrs={'class': 'form-check-input', 'id': 'active'})
+        # }
